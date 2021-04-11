6db5c3888a2e1df247ee628a31fb1438e6ac98aa
DoubleBrackets <arthurgan29@gmail.com>
2021-04-11 11:51:10 -0700

    unity gitignore added

--- GameJam Bullet Hell/Assets/Scripts/RoomTemplates.cs
+++ GameJam Bullet Hell/Assets/Scripts/RoomTemplates.cs
@@ -19,14 +19,14 @@ public class RoomTemplates : MonoBehaviour
 
     void Update() 
     {
-     if(waitTime <= 0 && spawnedBoss == false)
+        if(waitTime <= 0 && spawnedBoss == false)
         {
             for (int i = 0; i < rooms.Count; i++)
             {
                 if(i == rooms.Count - 1) 
                 {
                     Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
-                        spawnedBoss = true;
+                    spawnedBoss = true;
                 }
             }
         }
